37b862a9775804254d57181f1ea1022404958322
plainphw <s2028@e-mirim.hs.kr>
2022-08-17 10:11:26 +0900

    초기 커밋.

---
+++ CSClass2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Wanted<string> wanstedString = new Wanted<string>("String");
+            Wanted<int> wanstedInt = new Wanted<int>(52273);
+            Wanted<double> wanstedDouble = new Wanted<double>(52.273);
+            Console.WriteLine(wanstedString.Value);
+            Console.WriteLine(wanstedInt.Value);
+            Console.WriteLine(wanstedDouble.Value);
+        }
+    }
+}
